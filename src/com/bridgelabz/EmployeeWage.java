bddd734d2dd7a1ed1dc79e1c9421b89f1714f470
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-11 20:18:04 +0530

    update attendence

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,9 +1,18 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int IS_FULL_TIME = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to wage computation");
+
+        int empType = (int) (Math.random()*10) % 3;
+        if(empType == IS_FULL_TIME){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
 
     }
 }
